56bf419ce24c333cb2d8be6f7daee761d7312720
nataliaAvellaneda <nataliaprofedeinformatica@gmail.com>
2024-11-21 18:00:46 +0100

    actualización clase

---
+++ UT4/RecursosUT04AV/Ejercicio2.java
@@ -0,0 +1,27 @@
+package UT4/RecursosUT04AV;
+
+// Ejercicio 2.
+// A partir de estos dos arrays:
+// String[][] sopa = {
+// {"A", "E", "Y", "I", "V", "Y", "S", "A", "U", "R", "C", "W", "N", "M", "N", "N", "I", "H"},
+// {"X", "H", "C", "N", "B", "X", "D", "D", "W", "M", "A", "H", "H", "D", "U", "R", "B", "J"},
+// {"J", "K", "X", "I", "G", "E", "Z", "B", "Y", "K", "K", "O", "R", "E", "V", "K", "C", "V"},
+// {"S", "O", "A", "V", "O", "J", "L", "U", "G", "L", "C", "O", "Q", "F", "W", "W", "T", "Y"},
+// {"Q", "F", "L", "E", "L", "N", "F", "L", "D", "R", "Q", "R", "Y", "I", "W", "R", "K", "Q"},
+// {"D", "F", "D", "N", "B", "H", "O", "B", "S", "C", "I", "T", "O", "I", "G", "E", "B", "O"},
+// {"W", "I", "D", "U", "A", "Z", "O", "A", "M", "P", "I", "M", "B", "B", "C", "H", "Y", "B"},
+// {"M", "N", "X", "S", "T", "Z", "U", "S", "Q", "J", "R", "U", "E", "A", "A", "Q", "C", "Z"},
+// {"J", "G", "N", "A", "N", "O", "D", "A", "O", "F", "D", "O", "F", "R", "U", "T", "Y", "V"},
+// {"X", "H", "T", "U", "G", "A", "R", "U", "J", "H", "P", "N", "U", "G", "R", "H", "E", "C"},
+// {"C", "Z", "X", "R", "Q", "E", "B", "R", "Z", "U", "B", "A", "T", "T", "R", "J", "Z", "K"},
+// {"J", "S", "H", "Z", "U", "Y", "U", "D", "Z", "C", "D", "W", "R", "P", "L", "K", "Y", "B"}};
+// String[] palabras = {"BELLSPROUT", "BULBASAUR", "CROBAT", "GOLBAT", "GRIMER", "IVYSAUR", "KOFFING", "MUK", "VENUSAUR", "ZUBAT"};
+// Realiza un programa que muestre la sopa de letras y la lista de palabras 
+// contenidas. El usuario debe poder seleccionar la palabra que quiere encontrar. 
+// Tras la selección se redibujará la sopa de letras con las letras de la palabra 
+// seleccionada marcada en un color diferente. Ten en cuenta que las palabras 
+// solo pueden aparecer de izquierda a derecha, de arriba a abajo y en diagonal 
+// de izquierda-arriba a derecha-abajo.
+public class Ejercicio2 {
+
+}

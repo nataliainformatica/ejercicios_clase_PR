3f69abea4c78718d4ff35da5e4e4587495cfeb6b
nataliainformatica <113176659+nataliainformatica@users.noreply.github.com>
2024-12-03 17:28:39 +0100

    Update Ejercicio23.java

--- UT4/arraysIntermedio/ejercicio24/Ejercicio23.java
+++ UT4/arraysIntermedio/ejercicio24/Ejercicio23.java
@@ -3,10 +3,10 @@ package es.avellaneda;
 import java.util.Scanner;
 
 public class Ejercicio23 {
-
+    public static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
         // Permitir al usuario realizar las siguientes acciones:
-        Scanner sc = new Scanner(System.in);
+      
 
         Habitacion habitaciones[];
 
@@ -31,6 +31,18 @@ public class Ejercicio23 {
                     } else
                         System.out.println("No se ha podido reservar");
                     break; 
+
+                case "3":
+
+                    System.out.println("Introduce el número de habitación para cancelar la reserva");
+                   
+                    if(cancelarReserva(habitaciones, pedirNumero())){
+                        System.out.println("Se ha cancelado la reserva de la habitación" );
+                    }else{
+                        System.out.println("No se ha podido cancelar");
+                    }
+                    break;
+                    
                 case "0":
                     continua = false;
                     break;
@@ -44,11 +56,13 @@ public class Ejercicio23 {
     private static boolean reservarHabitacion(Habitacion[] habitaciones, String huesped) {
 
         // buscar la primera habitación libre
+
+
         for (Habitacion h : habitaciones) {
-            if (!h.mostrarEstado()) {
-                h.reservarHabitacion(huesped);
-                return true;
+            if (!h.consultarOcupacion()) {
+                return h.reservarHabitacion(huesped);            
             }
+
         }
         return false;
 
@@ -63,6 +77,14 @@ public class Ejercicio23 {
 
     }
 
+    private static Habitacion[] crearHabitaciones1(){
+        Habitacion[] habitaciones = new Habitacion[10];
+            for(int i=0; i<10; i++){
+                habitaciones[i]= new Habitacion();}
+                return habitaciones; 
+                
+    }
+
     private static Habitacion[] crearHabitaciones() {
         final int MAX = 10;
         Habitacion[] habitaciones = new Habitacion[MAX];
@@ -71,6 +93,43 @@ public class Ejercicio23 {
             habitaciones[i] = new Habitacion((i + 1));
         }
         return habitaciones;
+
+    }
+
+    private static boolean cancelarReserva(Habitacion[] habitaciones, int numero ){
+        boolean resultado = false; 
+
+            // es necesario buscar la instancia de habitacion
+
+            // no nos fiamos del índice del array, ya que podría ser que al cancelar reservas
+            // cambie el número respecto al índice
+        Habitacion habitacion = new Habitacion(numero);
+           for(Habitacion h: habitaciones){
+                if(h.equals(habitacion)){
+                     resultado = h.cancelarHabitacion();                   
+                }
+           }
+
+            
+            
+        return resultado; 
+    }
+
+    private static int pedirNumero(){
+        boolean continua = true;
+        int numero =0;
+        while(continua){
+
+            try{
+            numero = Integer.parseInt(sc.nextLine());
+            continua = false; 
+        }catch (Exception e) {
+            System.out.println("Debes introducir un número");
+            
+        }
+
+    }
+    return numero; 
 
     }
 

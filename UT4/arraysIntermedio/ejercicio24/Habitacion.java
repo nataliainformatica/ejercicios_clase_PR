223285f55322fbc02c8ced943be2795e09fc9a68
nataliainformatica <113176659+nataliainformatica@users.noreply.github.com>
2024-12-03 17:28:05 +0100

    Update Habitacion.java

--- UT4/arraysIntermedio/ejercicio24/Habitacion.java
+++ UT4/arraysIntermedio/ejercicio24/Habitacion.java
@@ -10,9 +10,19 @@ package es.avellaneda;
  */
 public class Habitacion {
 
+    private static int contador; 
+
     private int numero; 
     private String huesped; 
     private boolean ocupada; 
+
+    public Habitacion(){
+        this.numero = contador;
+        contador++; 
+        // escribo los requisitos del enunciado, si no tiene huesped debe ser null, no vacío 
+        this.huesped = null; 
+        ocupada = false;  
+    }
 
 
     public Habitacion(int numero){
@@ -37,8 +47,9 @@ public class Habitacion {
     // Cancelar una reserva, dejando la habitación vacía.
 
     public boolean cancelarHabitacion(){
-        boolean resultado = false; 
+        boolean resultado = true; // esto significa que ha podido cancelar la reserva
         if(!ocupada){
+            resultado = false; 
             return resultado;
         }
         huesped = null; 
@@ -60,15 +71,57 @@ public class Habitacion {
         return ocupada;
     }
 
+    
 
     @Override
     public String toString() {
-        String valor = "LIBRE";
-        if(ocupada){
-            valor = "OCUPADA";}
 
-        return "Habitacion numero= " + numero + "está "+ valor ;
-    }   
+        StringBuilder sb = new StringBuilder( "Habitacion numero = "); 
+        sb.append(numero); 
+        sb.append(" esta "); 
+
+        if(ocupada)
+            sb.append("OCUPADA POR " + huesped) ;
+        else{
+            sb.append("LIBRE");
+        } 
+
+        return  sb.toString();
+    }
+
+
+    public boolean isOcupada() {
+        return ocupada;
+    }
+
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + numero;
+        result = prime * result + ((huesped == null) ? 0 : huesped.hashCode());
+        result = prime * result + (ocupada ? 1231 : 1237);
+        return result;
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Habitacion other = (Habitacion) obj;
+        if (numero != other.numero)
+            return false;
+        
+        return true;
+    }
+
+
 
 
     
@@ -76,3 +129,4 @@ public class Habitacion {
 
 
 }
+

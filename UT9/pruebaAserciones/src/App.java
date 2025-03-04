6025271865d38e4070e374f3a3aec319982cc220
nataliainformatica <113176659+nataliainformatica@users.noreply.github.com>
2025-03-04 15:04:38 +0100

    Add files via upload

---
+++ UT9/pruebaAserciones/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        int edad = -5;
+        assert edad >= 0 : "La edad no puede ser negativa";
+       
+    }
+}
